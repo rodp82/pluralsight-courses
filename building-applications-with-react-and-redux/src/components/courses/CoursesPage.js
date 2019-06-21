b042bea0b1635143078a1571757b5e103a0b212a
Rod Pattison <rpattison@fmgl.com.au>
2019-06-21 11:06:45 +0800

    [9.13] mapDispatchToProps object form

--- building-applications-with-react-and-redux/src/components/courses/CoursesPage.js
+++ building-applications-with-react-and-redux/src/components/courses/CoursesPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import * as courseActions from '../../redux/actions/courseActions'
 
@@ -18,7 +17,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    this.props.actions.createCourse(this.state.course)
+    this.props.createCourse(this.state.course)
   }
 
   render() {
@@ -42,7 +41,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  createCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
@@ -51,10 +50,8 @@ function mapStateToProps(state) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  }
+const mapDispatchToProps = {
+  createCourse: courseActions.createCourse
 }
 
 export default connect(
