eb290f4df0401afd5e052a2cb1cbe1ba55c51231
Rod Pattison <rpattison@fmgl.com.au>
2019-07-05 14:41:44 +0800

    [10.7] Dispatched actions on load

--- building-applications-with-react-and-redux/src/components/courses/CoursesPage.js
+++ building-applications-with-react-and-redux/src/components/courses/CoursesPage.js
@@ -5,6 +5,12 @@ import PropTypes from 'prop-types'
 import * as courseActions from '../../redux/actions/courseActions'
 
 class CoursesPage extends React.Component {
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => {
+      alert('Loading courses failed' + error)
+    })
+  }
+
   render() {
     return (
       <>
